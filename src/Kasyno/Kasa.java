2e563c3b8fd56dd5994a0df31c9de1f5f384e565
Dreamcatcher94 <b1965501@trbvn.com>
2016-03-15 04:54:28 +0100

    commit

---
+++ src/Kasyno/Kasa.java
@@ -0,0 +1,32 @@
+package Kasyno;
+
+/**
+ * Created by Marta Pawlik on 21.01.2016.
+ */
+public class Kasa {
+
+    private static Kasa instance;
+
+    private double hajs;
+
+    public static Kasa getInstance() {
+
+        if (instance == null)
+            return instance = new Kasa();
+        else
+            return instance;
+    }
+
+    private Kasa() {
+        hajs = 9001.00;
+    }
+
+    public double getHajs() {
+        return hajs;
+    }
+
+    public double addMoney(double wartosc) {
+
+        return hajs = hajs + wartosc;
+    }
+}
---
+++ src/Kasyno/Main.java
@@ -0,0 +1,41 @@
+package Kasyno;
+
+import java.util.Scanner;
+
+/**
+ * Created by Marta Pawlik on 21.01.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        wyborGier();
+    }
+
+    public static void wyborGier() {
+
+        System.out.print("Wybierz tryb gry: \n\n1 - Jednoręki bandyta\n" +
+                "2 - Gra w  BlackJacka\n3 - Wyjście\n\nWybór: ");
+
+        Scanner s = new Scanner(System.in);
+
+        try {
+
+            switch (s.nextInt()) {
+
+                case 1:
+                    new JednorekiBandyta().start();
+                    break;
+
+                case 2:
+                    new BlackJack().start();
+                    break;
+
+                default:
+                    System.exit(0);
+            }
+        } catch (Exception e) {
+            System.exit(0);
+        }
+    }
+}
